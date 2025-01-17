f56a28df84ea6d3fc37444380f1370aa0737b3aa
babu <babu@admonk.in>
2025-01-17 13:24:09 +0530

    Order Page pick amount changes

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -32104,13 +32104,11 @@ $current_packed_balence = ($current_packed_balence > 0) ? $current_packed_balenc
        if($total_show_value==0)
        {
 
-            if($total_show_value>0)
-           {
+           
 
-              $this->db->query("UPDATE order_delivery_order_status SET collection_remarks_2='".round($total_show_value)."' WHERE order_id='".$_GET['order_id']."'  AND dispatch_status=0 AND deleteid=0");
+              $this->db->query("UPDATE order_delivery_order_status SET total_picked_amount='".round($total_show_value)."',collection_remarks_2='".round($total_show_value)."' WHERE order_id='".$_GET['order_id']."'  AND dispatch_status=0 AND deleteid=0 AND finance_status=2");
 
-           }
-
+          
 
        }
 
